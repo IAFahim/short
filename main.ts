9f27a60bc7b9b47a5b54f9436e8bc38a24c47dac
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-02-19 18:04:30 +0600

    Added api route

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-console.log(Deno.env.get("URL"));
+import {Application, Router} from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+const router = new Router();
+
+router.get("/", (context) => {
+    context.response.body = Deno.env.get("URL");
+})
