efea1f106a49409a52f07cd67ceca61f15af8be9
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-02-20 11:00:58 +0600

    checking

--- main.ts
+++ main.ts
@@ -3,8 +3,8 @@ import {Application, Router} from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 const router = new Router();
 
-router.get("/", (context) => {
-    context.response.body = Deno.env.get("URL");
+router.get("/:path", (context) => {
+    context.response.body = Deno.env.get("URL")+context.params.path;
 })
 
 app.use(router.routes());
