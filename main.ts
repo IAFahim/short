b87d18ed308f1909e119b7acccaf17c90dbc943d
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-02-20 11:13:47 +0600

    Checking global variable

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import {Application, Router} from "https://deno.land/x/oak/mod.ts";
+const URL= Deno.env.get("URL");
+const JWT= Deno.env.get("JWT");
 
 const app = new Application();
 const router = new Router();
 
 router.get("/:path", (context) => {
-    context.response.body = Deno.env.get("URL")+context.params.path;
+    context.response.body = URL + context.params.path+ "?token=" + JWT;
 })
 
 app.use(router.routes());
